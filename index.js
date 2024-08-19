b5cf7d50eb513bce3c2fdfca4f0bd60ab5f2a4a2
Gabe Harvey <harvey7@gmail.com>
2024-08-19 13:04:24 -0500

    React Test Code Written

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+ReactDOM.render(<p>Hello, Hi my name is Gabe!</p>, document.getElementById("root"))
+// ReactDOM.render(<p>Hello, Hi my name is Gabe and I enjoy using React!</p>, document.querySelector("#root"))
