399eee5b2a89c69a1fb02745b7cdcb3757f214c9
Gabe Harvey <harvey7@gmail.com>
2024-08-19 14:11:06 -0500

    JSX Basic

--- index.js
+++ index.js
@@ -68,10 +68,18 @@
 // )
 
 // Create an h1 element
-const h1 = document.createElement('h1');
+// const h1 = document.createElement('h1');
     
-// Set the text content
-h1.textContent = 'Hello, React';
+// // Set the text content
+// h1.textContent = 'Hello, React';
+// h1.className="header"
+// console.log(h1)
 
-// Append the h1 to the root element
-document.getElementById('root').appendChild(h1);
+// // Append the h1 to the root element
+// document.getElementById('root').appendChild(h1);
+
+const element = <h1 className='header'>This is JSX</h1>
+console.log(element)
+
+// JSX
+ReactDOM.render(element, document.getElementById('root'))
