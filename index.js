12d5cfc7c467e58045888d51241c27b498a7e3b6
Gabe Harvey <harvey7@gmail.com>
2024-08-19 13:08:16 -0500

    React Practice UL Items

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-ReactDOM.render(<p>Hello, Hi my name is Gabe!</p>, document.getElementById("root"))
+// ReactDOM.render(<p>Hello, Hi my name is Gabe!</p>, document.getElementById("root"))
 // ReactDOM.render(<p>Hello, Hi my name is Gabe and I enjoy using React!</p>, document.querySelector("#root"))
+
+// Unordered List Practice
+ReactDOM.render(<ul>
+    <li>List Item 1</li>
+    <li>List Item 2</li>
+    <li>List Item 3</li>
+</ul>, document.getElementById("root"))
