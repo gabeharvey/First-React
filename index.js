306a9c0d8dd1e3e486c086346a1da6496a3f6b28
Gabe Harvey <harvey7@gmail.com>
2024-08-19 20:28:22 -0500

    First challenge completed

--- index.js
+++ index.js
@@ -185,6 +185,14 @@
 const MyPage = (
     <div>
         <img src="./react-logo.png" width="40px" />
+        <h1>Fun Facts About React</h1>
+        <ul>
+            <li>Was First Released in 2013</li>
+            <li>Was originally created by Jordan Wilke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 );
 
